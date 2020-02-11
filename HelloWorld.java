bc95b96925549464f1d678a1c4a46ee19efc7d35
violetone <18848873370@163.com>
2020-02-11 23:00:58 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+		System.out.println("Hello World222222");
+		System.out.println("Hello World333333333333");
+		System.out.println("Hello World444444444444444444");
+		System.out.println("Hello World555555555555555555555555555");
+    }
+}
